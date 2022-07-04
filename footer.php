126ec3d070e922ab6910656d0c83e52533e8e3eb
Hasib144 <55952016+Hasib144@users.noreply.github.com>
2022-07-04 23:31:00 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,96 @@
+</div>
+<div class="container-fluid"></div>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<div class="sectionfooter ">
+
+	<div class="footer sfooter ">
+	<div class="col-lg-3 col-sm-3 col-xs-6"></div> <!-- Blank Div for positioning -->
+			<div class="col-lg-2  col-sm-2 col-xs-6">
+				<h3 class="check">Check Our Services</h3>
+				<ul class="products-footer check">
+
+				<li><a href="">Seat Cover</a></li>
+				
+		        <li><a href="">Turbo</a></li>
+				<li><a href="">Transmission</a></li>
+				
+               	<li><a href="">Tires</a></li>
+               
+                <li><a href="">Roof</a></li>
+                <li><a href="">Spoiler</a></li>
+                
+                <li><a href="">Horn</a></li>
+                
+				</ul>
+			</div>
+			 <div class="col-lg-3 col-sm-3 col-xs-4">
+			 <!-- <h3 class="check"> Find us on </h3>
+				<ul class="social">
+					<li><a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+					<li><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"></i></a></li>
+					
+					<li><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter fa-2x"></i></a></li>
+					<li><a href="https://www.youtube.com/channel/" target="_blank"><i class="fab fa-youtube fa-2x"></i></a></li>
+					<li><a href="https://github.com/" target="_blank"><i class="fab fa-github fa-2x"></i></a></li>
+					<li><a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></li>
+					<li><a href="https://plus.google.com" target="_blank"><i class="fab fa-google-plus fa-2x"></i></a></li>
+
+				</ul> -->
+
+				 <!-- <h3 class="check">Our Services</h3>
+				 <ul class="customer check">
+					<li><a href="">Book A Consultation</a></li>
+					<li><a href="javascript:void(Tawk_API.toggle())">Email Us</a></li>
+					<li><a href="javascript:void(Tawk_API.toggle())">Report A Problem</a></li>
+				</ul>  -->
+				
+				 
+			 </div> 
+			<div class="col-lg-3  col-sm-3 col-xs-12">
+				<h3 class="check">Contact Us</h3>
+				<ul class="contact ">
+					<li><p>Phone: +880-017xxxxxxxx</p></li>
+					<li><p>E-mail: <a href="mailto:md.hasib.khan@g.bracu.ac.bd">md.hasib.khan@g.bracu.ac.bd</a></p></li>
+					<li><p>Location : Jatrabari, <br>North Jatrabari, Dhaka, Bangladesh.</p></li>
+				</ul>
+				<h3 class="check">Payment Method</h3>
+				<ul class="cards">
+					<li><i class="fab fa-cc-visa fa-3x"></i></li>
+					<li><i class="fab fa-cc-mastercard fa-3x"></i></li>
+					<li><i class="fab fa-cc-paypal fa-3x"></i></li>
+					
+				</ul>
+
+               
+				
+				
+			</div>
+			<div></div>
+
+
+
+
+
+			<div class="col-lg-12 col-sm-12 col-xs-12 copyright check"><p>&copy; <?php echo date('Y'); ?> KHANz Automobile</p></div>
+	</div>
+
+
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/606cb8f8067c2605c0bfd6c8/1f2kb5dve';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</script>
+<!--End of Tawk.to Script-->
+</div>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+   $title = "Advance Auto Parts"; 
+   include ('header.php') ;
+  ?>
+<script type="text/javascript" src="js/count.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<link rel="stylesheet" type="text/css" href="css/exhibitor.css">
+<script type="text/javascript" src="js/menu.js"></script>
+<link href='https://fonts.googleapis.com/css?family=Gafata|Roboto:700' rel='stylesheet' type='text/css'>
+
+<header class="videohead">
+
+ 	
+    <video class="video-bg" autoplay muted loop>
+
+		<source src="img/KHANz.mp4" type="video/webm">
+        <source src="img/KHANz.mp4" type="video/mp4">
+        <source src="img/preview.ogv" type="video/ogg">
+	</video> 
+
+	<div class="video-overlay">
+		<h1 class="shadow container">KHANz Automobile </h1>
+
+    <style>
+body {
+  background-color: burlywood;
+  
+  } 
+</style>
+    
+    
+    
+        
+        <a href="booking.php" class="btn btn-primary mianbutton container" style="text-align: center;">Find Your Friendly Neighbourhood Mechanic Here</a>
+	</div>
+
+</header>
+
+
+
+
+
+
+
+
+
+
+<?php
+
+   include ('footer.php') ;
+  ?>
+
+
